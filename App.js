c51f62798ec66fe64130ed9f1996dae03cd9a4f3
dorado5115 <doradodx@gmail.com>
2022-03-31 15:18:17 -0600

    animate

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Animated, Button } from 'react-native';
 
 export default function App() {
+  const marginTop = new Animated.Value(20);
+
+  const animate = () => {
+    Animated.timing(marginTop, {
+      toValue: 200,
+      duration: 500,
+      useNativeDriver: false
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Animate" onPress={animate} />
       <StatusBar style="auto" />
+      <Animated.View style={{ marginTop }} >
+        <Text style={styles.text}>Alooo policia</Text>
+      </Animated.View>
     </View>
   );
 }
@@ -17,4 +30,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  text: {
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+
 });
